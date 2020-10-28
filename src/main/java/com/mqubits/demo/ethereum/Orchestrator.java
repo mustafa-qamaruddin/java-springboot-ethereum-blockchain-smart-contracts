f62f5aaf268e92c2b01a01cd86aac21e109e0a5b
Mustafa Qamaruddin <m.qamaruddin.89@gmail.com>
2020-10-28 18:07:08 +0200

    Add Web3j initializer

--- src/main/java/com/mqubits/demo/ethereum/Orchestrator.java
+++ src/main/java/com/mqubits/demo/ethereum/Orchestrator.java
@@ -1,5 +1,15 @@
 package com.mqubits.demo.ethereum;
 
+import java.io.IOException;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.protocol.core.methods.response.EthBlockNumber;
+import org.web3j.protocol.core.methods.response.EthGasPrice;
+import org.web3j.protocol.core.methods.response.Web3ClientVersion;
+
 public class Orchestrator {
-    
+    public void init() {
+        Web3j web3 = Web3j.build(new HttpService("<NODE ENDPOINT>"));
+
+    }
 }
