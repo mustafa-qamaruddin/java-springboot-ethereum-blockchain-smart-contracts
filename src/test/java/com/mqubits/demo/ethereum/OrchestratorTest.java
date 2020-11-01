a2fee76f6ec3c6e91074b96e34f196d2b12c5270
Mustafa Qamar-ud-Din <mustafa@mQuBits.com>
2020-11-01 12:17:47 +0200

    Add Ethereum EVM mock server for unit and integration tests

--- src/test/java/com/mqubits/demo/ethereum/OrchestratorTest.java
+++ src/test/java/com/mqubits/demo/ethereum/OrchestratorTest.java
@@ -1,9 +1,11 @@
 package com.mqubits.demo.ethereum;
 
 import org.junit.jupiter.api.Test;
+import org.web3j.EVMTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@EVMTest
 public class OrchestratorTest {
   @Test
   void testInit() {
